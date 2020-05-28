3a3c4bc8b643383cbdee6c015735df9d40bfcde4
jakesj425 <jakesj425@users.noreply.github.com>
2020-05-28 19:57:39 +0200

    Update beep.ps1

--- beep.ps1
+++ beep.ps1
@@ -1,3 +1,3 @@
 Add-Type -AssemblyName System.Speech 
 $synth = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
-$synth.Speak('Beep, Boop, Beep, Testing powershell talk?')
+$synth.Speak('Beep, Boop, Beep, Testing powershell talk!, This synth instruction was downloaded from the big bad internet')
